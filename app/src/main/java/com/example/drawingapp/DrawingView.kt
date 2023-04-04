7896dcc8b8b2cd068ad0caa1106d4399ce58dd3d
Chirag Bhalotia <me@chirag.codes>
2023-04-05 00:21:13 +0530

    progress 2

--- app/src/main/java/com/example/drawingapp/DrawingView.kt
+++ app/src/main/java/com/example/drawingapp/DrawingView.kt
@@ -3,70 +3,107 @@ package com.example.drawingapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
-class DrawingView(context:Context,attrs:AttributeSet):View(context,attrs) {
-    private var mDrawPath: CustomPath? = null
-    private var mBitMap: Bitmap? = null
-    private var mDrawPaint:Paint? = null
-    private var mCanvasPaint:Paint? = null
-    private var mBrushSize:Float = 20.0f
+
+class DrawingView(context: Context,attrs:AttributeSet): View(context,attrs) {
+    private var mDrawPath: CustomPath? = null // Stores the current path that is being drawn on the screen
+    private var mCanvasBitmap: Bitmap? = null // An instance of the Bitmap.
+
+    private var mDrawPaint: Paint? =
+        null // The Paint class holds the style and color information about how to draw geometries, text and bitmaps.
+    private var mCanvasPaint: Paint? = null // Instance of canvas paint view.
+
+    private var mBrushSize: Float = 0f
     private var color = Color.BLACK
-    private var canvas:Canvas? = null
+
+    private var canvas: Canvas? = null
+
+    private val mPaths = ArrayList<CustomPath>() // Stores all the paths that was drawn on the screen
 
     init {
-        setupDrawing()
+        setUpDrawing()
     }
 
-    private fun setupDrawing(){
+    private fun setUpDrawing() {
         mDrawPaint = Paint()
-        mDrawPath = CustomPath(color,mBrushSize)
-        mDrawPaint!!.color = color
-        mDrawPaint!!.style = Paint.Style.STROKE
-        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
-        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
-        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+        mDrawPath = CustomPath(color, mBrushSize)
+        mDrawPaint?.color = color
+        mDrawPaint?.style = Paint.Style.STROKE // This is to draw a STROKE style
+        mDrawPaint?.strokeJoin = Paint.Join.ROUND // This is for store join
+        mDrawPaint?.strokeCap = Paint.Cap.ROUND // This is for stroke Cap
+
+        mCanvasPaint = Paint(Paint.DITHER_FLAG) // Paint flag that enables dithering when blitting.
+
     }
 
-    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        super.onSizeChanged(w, h, oldw, oldh)
-        mBitMap = Bitmap.createBitmap(w,h,Bitmap.Config.ARGB_8888)
-        canvas = Canvas(mBitMap!!)
+    override fun onSizeChanged(w: Int, h: Int, wprev: Int, hprev: Int) {
+        super.onSizeChanged(w, h, wprev, hprev)
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(mCanvasBitmap!!)
     }
 
-    override fun onDraw(canvas: Canvas?) {
+
+
+    override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        canvas?.drawBitmap(mBitMap!!,0.0f,0.0f,mCanvasPaint)
-        if (!mDrawPath!!.isEmpty){
-            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
-            mDrawPaint!!.color = mDrawPath!!.color
+
+        mCanvasBitmap?.let {
+            canvas.drawBitmap(it, 0f, 0f, mCanvasPaint)
+        }
+
+
+        for (p in mPaths) {
+            mDrawPaint?.strokeWidth = p.brushThickness
+            mDrawPaint?.color = p.color
+            canvas.drawPath(p, mDrawPaint!!)
+        }
+
+        if (!mDrawPath!!.isEmpty) {
+            mDrawPaint?.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint?.color = mDrawPath!!.color
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
         }
     }
 
+
     override fun onTouchEvent(event: MotionEvent): Boolean {
-        println(event.action)
-        var touchX = event.x
-        var touchY = event.x
-        when(event.action){
+        val touchX = event.x // Touch event of X coordinate
+        val touchY = event.y // touch event of Y coordinate
+
+        when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-                mDrawPath!!.color = color
-                mDrawPath!!.brushThickness = mBrushSize
-                mDrawPath!!.reset()
-                mDrawPath!!.moveTo(touchX,touchY)
+                mDrawPath?.color = color
+                mDrawPath?.brushThickness = mBrushSize
+                mDrawPath?.reset() // Clear any lines and curves from the path, making it empty.
+                mDrawPath?.moveTo(
+                    touchX,
+                    touchY
+                ) // Set the beginning of the next contour to the point (x,y).
             }
             MotionEvent.ACTION_MOVE -> {
-                mDrawPath!!.lineTo(touchX, touchY)
+                mDrawPath?.lineTo(
+                    touchX,
+                    touchY
+                ) // Add a line from the last point to the specified point (x,y).
             }
             MotionEvent.ACTION_UP -> {
-                mDrawPath = CustomPath(color,mBrushSize)
+                mPaths.add(mDrawPath!!) //Add when to stroke is drawn to canvas and added in the path arraylist
+                mDrawPath = CustomPath(color, mBrushSize)
             }
             else -> return false
         }
-//        invalidate()
+
+        invalidate()
         return true
     }
 
-    internal inner class CustomPath(var color: Int, var brushThickness:Float): Path(){
-
+    fun setBrushSize(newSize:Float){
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,newSize,resources.displayMetrics)
     }
+
+
+
+    internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path()
 }
--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawingView: DrawingView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        drawingView = findViewById<DrawingView>(R.id.drawingView)
+        drawingView.setBrushSize(20f)
+
+
     }
 }
