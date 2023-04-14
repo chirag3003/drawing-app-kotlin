258df85324df62fb25bc1fd079390a55a640451c
Chirag Bhalotia <me@chirag.codes>
2023-04-15 05:15:37 +0530

    added permission for storage

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -1,17 +1,38 @@
 package com.example.drawingapp
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
     private lateinit var drawingView: DrawingView
-    private lateinit var imageButtonCurrentPaint:ImageButton
+    private lateinit var imageButtonCurrentPaint: ImageButton
+    private val activityLauncher: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+            permissions ->
+                permissions.entries.map {
+                    val pName = it.key
+                    val pIsGranted = it.value
+                    println(pIsGranted)
+                    if(pIsGranted){
+                        Toast.makeText(this,"Permission Granted",Toast.LENGTH_LONG).show()
+                    } else
+                        Toast.makeText(this,"Permission Not Granted", Toast.LENGTH_SHORT).show()
+                }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,31 +42,41 @@ class MainActivity : AppCompatActivity() {
         val linearLayoutColors = findViewById<LinearLayout>(R.id.ll_paint_colors)
 
         imageButtonCurrentPaint = linearLayoutColors[1] as ImageButton
-        imageButtonCurrentPaint.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.pallet_pressed))
+        imageButtonCurrentPaint.setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.pallet_pressed
+            )
+        )
 
         val brushBtn = findViewById<ImageButton>(R.id.ib_brush)
-        brushBtn.setOnClickListener{
+        brushBtn.setOnClickListener {
             showBrushSizeDialog()
+        }
+
+        val galleryBtn = findViewById<ImageButton>(R.id.ib_gallery)
+        galleryBtn.setOnClickListener(){
+            requestPermission()
         }
 
     }
 
-    private fun showBrushSizeDialog(){
+    private fun showBrushSizeDialog() {
         val brushD = Dialog(this)
         brushD.setContentView(R.layout.dialog_brush_size)
         brushD.setTitle("Brush Size: ")
-        val smBtn:ImageButton  = brushD.findViewById(R.id.ib_small_brush)
-        val mdBtn  = brushD.findViewById<ImageButton>(R.id.ib_medium_brush)
-        val lgBtn  = brushD.findViewById<ImageButton>(R.id.ib_large_brush)
+        val smBtn: ImageButton = brushD.findViewById(R.id.ib_small_brush)
+        val mdBtn = brushD.findViewById<ImageButton>(R.id.ib_medium_brush)
+        val lgBtn = brushD.findViewById<ImageButton>(R.id.ib_large_brush)
         smBtn.setOnClickListener() {
             drawingView.setBrushSize(10f)
             brushD.dismiss()
         }
-        mdBtn.setOnClickListener{
+        mdBtn.setOnClickListener {
             drawingView.setBrushSize(20f)
             brushD.dismiss()
         }
-        lgBtn.setOnClickListener{
+        lgBtn.setOnClickListener {
             drawingView.setBrushSize(30f)
             brushD.dismiss()
         }
@@ -53,14 +84,41 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    private fun requestPermission(){
+        println("it was called")
+        if (shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE)){
+            Toast.makeText(this,"Yes", Toast.LENGTH_SHORT).show()
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Permission Required");
+            builder.setMessage("We need media permissions for this feature")
+            builder.setNeutralButton("Cancel"){dialog,_ ->
+                dialog.dismiss()
+            }
+            builder.show()
+            println("yeah")
+        } else {
+            println("Valid")
+            activityLauncher.launch(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE))
+        }
+    }
 
-    fun paintClicked(view: View){
-        if(view != imageButtonCurrentPaint){
-            imageButtonCurrentPaint.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.pallet_normal))
+    fun paintClicked(view: View) {
+        if (view != imageButtonCurrentPaint) {
+            imageButtonCurrentPaint.setImageDrawable(
+                ContextCompat.getDrawable(
+                    this,
+                    R.drawable.pallet_normal
+                )
+            )
             imageButtonCurrentPaint = view as ImageButton
             val cTag = imageButtonCurrentPaint.tag.toString()
             drawingView.setColor(cTag)
-            imageButtonCurrentPaint.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.pallet_pressed))
+            imageButtonCurrentPaint.setImageDrawable(
+                ContextCompat.getDrawable(
+                    this,
+                    R.drawable.pallet_pressed
+                )
+            )
         }
     }
 
